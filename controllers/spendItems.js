1ea4e17da89710d9c3ccf4106b55e66e2d8f5c7e
Mohit Bajaj <mohit3@Mohits-Macbook.local>
2019-08-16 18:13:12 +0530

    Added Add Spend and Edit Spend APIs

---
+++ controllers/spendItems.js
@@ -0,0 +1,89 @@
+const addSpend = (req, res, db) => {
+	// Get data from body
+	let spend = {
+		name: req.body.name,
+		amount: req.body.amount,
+		category_id: req.body.categoryId,
+		sub_category_id: req.body.subCategoryId,
+		purchase_date: req.body.purchaseDate,
+	}
+	
+	// Validate token
+	db.select('user_id').from('users')
+		.where('token', '=', req.body.token)
+		.then(data => {
+			if(data.length){
+				spend["user_id"] = Number(data[0]["user_id"]);
+				// enter spend into database 
+				db('spend_items')
+					.returning('*')
+					.insert(spend)
+					.then((data) => {
+						let insertedSpend = data[0];
+						res.status(201).json(insertedSpend);
+					})
+					.catch(err => {
+						console.log(err);
+						res.status(400).json("Could not add spend to database");
+					})
+			} else {
+				res.status(400).json("Invalid token. Please log out of the app and sign in again.")
+			}
+		})
+
+}
+
+const editSpend = (req, res, db) => {
+	let spend = {
+		name: req.body.name,
+		amount: req.body.amount,
+		category_id: req.body.categoryId,
+		sub_category_id: req.body.subCategoryId,
+		purchase_date: req.body.purchaseDate,
+	}
+
+	// Validate token
+	db.select('user_id').from('users')
+		.where('token', '=', req.body.token)
+		.then(data => {
+			if(data.length){
+				let userId = Number(data[0]["user_id"]);
+				// spend["user_id"] = Number(data[0]["user_id"]);
+				//Check is the user editing the spend has created it
+				db.select('user_id').from('spend_items')
+					.where('id', '=', req.params.id)
+					.then((data) => {
+						if(data.length && userId == data[0]["user_id"]) {
+							// enter spend into database 
+							db('spend_items')
+								.where("id", "=", req.params.id)
+								.returning('*')
+								.update(spend)
+								.then(data => {
+									let updatedSpend = data[0];
+									res.status(200).json(updatedSpend);
+								})
+								.catch(err => {
+									console.log(err);
+									res.status(400).json("Update failed");
+								})
+						} else {
+							res.status(400).json("You do not have permission to edit this spend");
+						}
+					})
+					.catch(err => {
+						res.status(400).json("Something went wrong");
+					});		
+			} else {
+				res.status(400).json("Invalid token. Please log out of the app and sign in again.");
+			}
+		})
+
+}
+
+module.exports = {
+	addSpend: addSpend,
+	editSpend: editSpend
+	/* listSpends: listSpends,
+	deleteSpend: deleteSpend */
+};	
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const saltRounds = config.SALT_ROUNDS;
 
 const registerController = require('./controllers/register');
 const loginController = require('./controllers/login');
+const spendsController = require('./controllers/spendItems');
 
 const db = knex(config.DB_CREDENTIALS);
 
@@ -25,6 +26,8 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => { registerController.handleRegister(req, res, db, bcrypt, saltRounds) });
 app.post('/login', (req, res) => { loginController.handleLogin(req, res, db, bcrypt) });
+app.post('/spend', (req, res) => { spendsController.addSpend(req, res, db) });
+app.put('/spend/:id', (req, res) => { spendsController.editSpend(req, res, db) });
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
