f6b8f5f859becb9e3591018c45db83351f5d1082
Mohit Bajaj <mohit_b35@hotmail.com>
2020-01-24 11:26:11 +0530

    Update create token to make tokens url safe

--- controllers/utilities.js
+++ controllers/utilities.js
@@ -4,7 +4,9 @@ const crypto = require('crypto');
 const createToken = () => {
 	return new Promise((resolve, reject) => {
 	  crypto.randomBytes(32, (err, data) => {
-		err ? reject(err) : resolve(data.toString('base64'));
+		err ? 
+			reject(err) : 
+			resolve(data.toString('base64').replace(/\//g,'_').replace(/\+/g,'-')); //Replacing / & + to make tokens url safe
 	  })
 	})
   }
