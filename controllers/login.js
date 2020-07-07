dcdc0bc5ae699a684516c2372832269a264a2f4d
Mohit Bajaj <mohit_b35@hotmail.com>
2020-07-07 18:48:58 +0530

    refactor handleLogin to use async await - instead of promise chaining

--- controllers/login.js
+++ controllers/login.js
@@ -1,65 +1,79 @@
 const utilities = require('./utilities');
 
-const handleLogin = (req, res, db, bcrypt) => {
+const handleLogin = async (req, res, db, bcrypt) => {
 	// Get the details from the body
 	let { email, password } = req.body;
 
 	// Check if email exists in the database, and get corresponding user ID
-	db.select('user_id').from('users')
-		.where('email', '=', email)
-		// For user ID returned, check if the user hash matches with password using bcrypt.compare
-		.then(data => {
-			if(data.length) {
-				let userId = data[0].user_id;
-				db.select('hash').from('user_credentials')
-					.where('id','=',userId)
-					.then(data => {
-						let { hash } = data[0];
-						bcrypt.compare(password, hash)
-							.then((result) => {
-								// If hash matches, generate a new token, insert for new user and then return user details
-								if(result){
-									utilities.createToken()
-									.then((token) => {
-										db('users')
-											.where('user_id', '=', userId)
-											.returning('*')
-											.update({
-												'token': token,
-												'last_login': new Date()
-											})
-											.then(data => {
-												let user = data[0];
-												res.status(200).json(user);
-											})
-											.catch((err) => {
-												res.status(400).json("4. Error retrieving your data. Something went wrong.");
-											})
-									})
-									.catch((err) => {
-										res.status(400).json("5. Error retrieving your data. Something went wrong");
-									})	
-								} else {
-									res.status(400).json("3. Invalid credentials. Check your email and password.");
-								}
-							})
-							.catch((err) => {
-								res.status(400).json("7. Error verifying credentials. Something went wrong");
-							})
-					})
-					.catch((err) => {
-						res.status(400).json("6. Error verifying your credentials. Something went wrong.")
-					})
-			} else {
-				res.status(400).json("2. Invalid credentials. Check your email and password.");
-			}
-		})
-		.catch((err) => {
-			res.status(400).json("1. Error retrieving your data. Something went wrong.");
-		})
+	let userData;
+	try {
+		userData = await db.select('user_id').from('users').where('email', '=', email);
+	} catch {
+		return res.status(400).json("1. Error retrieving your data. Something went wrong."); 
+	}
 	
+	// Return error if user does not exist
+	if (!userData.length){
+		return res.status(400).json("2. Invalid credentials. Check your email and password.");
+	} else {
+		let userId = userData[0].user_id;
+
+		// Fetch hash for the fetched user ID
+		let hashData;
+		try {
+			hashData = await db.select('hash').from('user_credentials').where('id','=', userId);
+		} catch {
+			return res.status(400).json("3. Error retrieving your data. Something went wrong.");
+		}
+
+		if(hashData.length) {
+			let { hash } = hashData[0];
+			// Verify if hash matches with password using bcrypt.compare
+			let pwdVerified;
+			try {
+				pwdVerified = await bcrypt.compare(password, hash);
+			} catch {
+				return res.status(400).json("5. Error verifying credentials. Something went wrong.");
+			}
+			
+			// If hash matches (result is true), generate a new token, insert for new user and then return user details
+			if (pwdVerified) {
+				let token, userInfo;
+
+				try {
+					token = await utilities.createToken();
+				} catch {
+					return res.status(400).json("7. Error verifying credentials. Something went wrong.")
+				}
+				
+				try {
+					userInfo = await db('users')
+						.where('user_id', '=', userId)
+						.returning('*')
+						.update({
+							'token': token,
+							'last_login': new Date()
+						});
+				} catch {
+					return res.status(400).json("8. Error retrieving your data. Something went wrong");
+				}
+				
+				if (userInfo.length) {
+					let user = userInfo[0];
+					return res.status(200).json(user);
+				} else {
+					return res.status(400).json("9. Error retrieving your data. Something went wrong.");
+				}	
+			} else {
+				return res.status(400).json("6. Invalid credentials. Check your email and password.");
+			}
+		} else {
+			return res.status(400).json("4. Error retrieving your data. Something went wrong.");
+		}
+	}
 }
 
+		
 // Validates user ID and clears token if validation successful
 const handleLogout = (req, res, db) => {
 	let { userId,token } = req.body;
